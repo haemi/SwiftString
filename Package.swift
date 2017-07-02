eee4d6ecbb50f019e6f8f0fa8c7c41c3e7640651
Stefan Walkner <stefan@arkulpa.at>
2017-07-02 13:35:21 +0200

    fix 'argument \'swiftLanguageVersions\' must precede argument \'dependencies\''

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftString",
     targets: [],
-    dependencies: [],
 	swiftLanguageVersions: [3],
+    dependencies: [],
     exclude: []
 )
